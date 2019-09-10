9ebb70645d34a73e4a77594283509c65b9077a97
Xiaoting Chen <xiaoting.chen@cchmc.org>
2019-09-10 09:19:38 -0400

    .

--- manuscripts/genome_research2018/get_model.sh
+++ manuscripts/genome_research2018/get_model.sh
@@ -3,3 +3,7 @@
 wget https://storage.googleapis.com/131k/model.tf.index
 wget https://storage.googleapis.com/131k/model.tf.meta
 wget https://storage.googleapis.com/131k/model.tf.data-00000-of-00001
+
+wget https://storage.googleapis.com/basenji_transfer_learning/model.tf.data-00000-of-00001
+wget https://storage.googleapis.com/basenji_transfer_learning/model.tf.index
+wget https://storage.googleapis.com/basenji_transfer_learning/model.tf.meta
